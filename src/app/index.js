0bc53a01374d744bc3f3b84c767b0ff759b05e78
giovana1806 <betareligiovana@gmail.com>
2024-05-14 16:46:49 -0300

    wifi conected

---
+++ src/app/index.js
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from "react";
+import {View} from 'react-native';
+import Aviso from "./components/aviso";
+import NetInfo from '@react-native-community/netinfo';
+
+export default function App() {
+  const [isConnected, setIsConnected] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setIsConnected(state.isConnected);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      {isConnected === null ? (
+        <Aviso texto="Verificando conexão..." isConnected={isConnected}/>
+      ) : isConnected ? (
+        <Aviso texto="Conectado á internet" isConnected={isConnected}/>
+      ) : (
+        <Aviso text="Desconectado da internet" isConnected={isConnected}/>
+      )}
+    </View>
+  );
+}
+
